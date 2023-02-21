c9826ab86cc76b4e7bf74e8d5896396658ebd81e
jonathan <joncanales2@gmail.com>
2023-02-21 13:16:27 -0500

    added baseline dependencies for project.

---
+++ checkly.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "@checkly/cli";
+
+export default defineConfig({
+  projectName: "Website Monitoring",
+  logicalId: "website-monitoring-1",
+  repoUrl: "https://github.com/acme/website",
+  checks: {
+    activated: true,
+    muted: false,
+    runtimeId: "2022.10",
+    frequency: 5,
+    locations: ["us-east-1", "eu-west-1"],
+    tags: ["website", "api"],
+    alertChannels: [],
+    checkMatch: "**/__checks__/*.check.ts",
+    ignoreDirectoriesMatch: [],
+    browserChecks: {
+      frequency: 10,
+      testMatch: "**/__checks__/*.spec.ts",
+    },
+  },
+  cli: {
+    runLocation: "eu-west-1",
+  },
+});

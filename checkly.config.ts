6b2f8867979f5db8250b7808f1374c18607ff802
jonathan <joncanales2@gmail.com>
2023-03-01 14:16:38 -0500

    removed redundant config setup.

--- checkly.config.ts
+++ checkly.config.ts
@@ -1,7 +1,9 @@
-const config = {
-  projectName: "Website Monitoring",
-  logicalId: "website-monitoring-1",
-  repoUrl: "https://github.com/acme/website",
+import { defineConfig } from '@checkly/cli'
+
+const config = defineConfig({
+  projectName: 'Advanced Example Project',
+  logicalId: 'advanced-example-project-1',
+  repoUrl: 'https://github.com/checkly/checkly-cli',
   checks: {
     activated: true,
     muted: false,
@@ -18,8 +20,38 @@ const config = {
     },
   },
   cli: {
-    runLocation: "eu-west-1",
+    runLocation: 'eu-west-1',
   },
-};
+})
 
 export default config
+
+// ORIGINAL
+
+// import { defineConfig } from '@checkly/cli'
+
+// const config = defineConfig({
+//   projectName: "Website Monitoring",
+//   logicalId: "website-monitoring-1",
+//   repoUrl: "https://github.com/acme/website",
+//   checks: {
+//     activated: true,
+//     muted: false,
+//     runtimeId: "2022.10",
+//     frequency: 5,
+//     locations: ["us-east-1", "eu-west-1"],
+//     tags: ["website", "api"],
+//     alertChannels: [],
+//     checkMatch: "**/__checks__/*.check.ts",
+//     ignoreDirectoriesMatch: [],
+//     browserChecks: {
+//       frequency: 10,
+//       testMatch: "**/__checks__/*.spec.ts",
+//     },
+//   },
+//   cli: {
+//     runLocation: "eu-west-1",
+//   },
+// });
+
+// export default config

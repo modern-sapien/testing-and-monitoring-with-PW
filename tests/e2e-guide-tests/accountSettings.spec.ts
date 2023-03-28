a250fc6e2a1cf7c8b20e8064f9fe0b5cf83db709
jonathan <joncanales2@gmail.com>
2023-03-27 23:32:17 -0400

    updated filemgmt spec to use more recent locators & refactored to be simpler in approach.

--- tests/e2e-guide-tests/accountSettings.spec.ts
+++ tests/e2e-guide-tests/accountSettings.spec.ts
@@ -4,15 +4,15 @@ import { test, expect } from "@playwright/test";
 test("account settings", async ({ page }) => {
   await page.goto("https://danube-web.shop/");
 
-  await page.click("#login");
-  await page.click("#n-email");
-  // @ts-ignore USER_EMAIL is returning a string
-  await page.type("#n-email", process.env.USER_EMAIL);
-  // @ts-ignore USER_PASSWORD is returning a string
-  await page.type("#n-password2", process.env.USER_PASSWORD);
-  await page.click("#goto-signin-btn");
+  await page.locator("#login").click();
 
-  await page.click(".fa-user");
+  // @ts-ignore USER_EMAIL is returning a string
+  await page.locator("#n-email").type(process.env.USER_EMAIL);
+  // @ts-ignore USER_PASSWORD is returning a string
+  await page.locator("#n-password2").type(process.env.USER_PASSWORD);
+  await page.locator("#goto-signin-btn").click();
+
+  await page.locator(".fa-user").click();
 
   await page.waitForSelector("#user-details > div > input");
 
--- tests/e2e-guide-tests/fileMgmt.spec.ts
+++ tests/e2e-guide-tests/fileMgmt.spec.ts
@@ -2,78 +2,51 @@ import { test, expect } from "@playwright/test";
 const fs = require("fs");
 const axios = require("axios");
 
-
 test.beforeEach(async ({ page }) => {
   await page.goto("https://danube-web.shop/");
 
-  await page.click("#login");
+  await page.locator("#login").click();
   // @ts-ignore USER_EMAIL is returning a string
-  await page.type("#n-email", process.env.USER_EMAIL);
+  await page.locator("#n-email").type(process.env.USER_EMAIL);
   // @ts-ignore USER_PASSWORD is returning a string
-  await page.type("#n-password2", process.env.USER_PASSWORD);
+  await page.locator("#n-password2").type(process.env.USER_PASSWORD);
   await page.getByRole("button", { name: "Sign In" }).click();
 
   await page.locator("#account").click();
 });
 
 test("file upload", async ({ page }) => {
-  process.env.CHECKLY_DONT === "skip"?  
-    // @ts-ignore FILE_PATH is returning a string
-  page.locator('input[type="file"]'):page.locator('input[type="file"]').setInputFiles(process.env.FILE_PATH)
+  // @ts-ignore FILE_PATH is returning a string
+  await page.locator('input[type="file"]').setInputFiles(process.env.FILE_PATH);
 
   await page.getByRole("button", { name: "Upload" }).click();
 
-  await page.getByText("Upload successful.");
+  await expect(page.locator("#upload-message-succcess")).toHaveText("Upload successful.");
 });
 
-test.describe("download flow", () => {
-test.skip(process.env.CHECKLY_DONT === "skip")
 test("file download", async ({ page }) => {
-  // @ts-ignore HREF element does exist
-  const url = await page.$eval("#orders > ul > li > a", (el) => el.href);
+  const url = await page.locator("#orders > ul > li > a").getAttribute("href");
   const filePath = "./tests/fixtures/downloadedFile.pdf";
 
-  // Download the file using Axios
-  const response = await axios({
-    method: "get",
-    url: url,
-    responseType: "stream",
-  });
-  const writer = fs.createWriteStream(filePath);
-  response.data.pipe(writer);
+  const response = await axios.get("https://danube-web.shop" + url);
+  const downloadedFile = Buffer.from(response.data);
 
-  // Handle if writing passes or failes
-  await new Promise((resolve, reject) => {
-    writer.on("finish", resolve);
-    writer.on("error", reject);
-  });
+  const testFile = await fs.readFileSync(filePath);
 
-  // Compare the downloaded file to the existing file using Playwright/Test
-  // @ts-ignore file path is a string
-  const downloadedFile = await page.locator("input[type=file]").inputValue(filePath);
-  const existingFile = await page
-    .locator("input[type=file]")
-    // @ts-ignore TEST_FILE_PATH is a string
-    .inputValue(process.env.TEST_FILE_PATH);
-  expect(downloadedFile).toEqual(existingFile);
+  expect(downloadedFile.equals(testFile));
 });
 
 test("file download alt", async ({ page }) => {
-  await page.locator("#orders > ul > li:nth-child(1) > a");
-
   const downloadPromise = page.waitForEvent("download");
 
   await page.getByRole("link", { name: "Invoice" }).click();
 
   const download = await downloadPromise;
 
-  const newFilePath = await download.path();
+  const downloadFilePath = await download.path();
   // @ts-ignore TEST_FILE_PATH is a string
   const testFile = await fs.promises.readFile(process.env.TEST_FILE_PATH);
-  const newFile = await fs.promises.readFile(newFilePath);
+  const downloadedFile = await fs.promises.readFile(downloadFilePath);
 
-  expect(testFile).toEqual(newFile);
+  expect(testFile).toEqual(downloadedFile);
 });
-
-} )
-
